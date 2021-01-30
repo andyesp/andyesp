9639294c052b5d29dd1e5919795022c70ae6ec5f
Andrés Espagnolo <andyespagnolo@gmail.com>
2021-01-29 23:12:55 -0300

    Hover no more

--- pages/collectibles.js
+++ pages/collectibles.js
@@ -7,7 +7,7 @@ import { Collectibles as CollectiblesData } from '~/lib/data'
 
 const Collectibles = () => (
   <>
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {CollectiblesData.map((item) => (
         <CollectibleItem key={item.id} item={item} />
       ))}
